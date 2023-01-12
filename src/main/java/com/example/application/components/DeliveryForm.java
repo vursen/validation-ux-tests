89b205d97bb89a73672697471fdc8baa8035d1ba
Sergey Vinogradov <mr.vursen@gmail.com>
2023-01-12 12:34:53 +0200

    change suitable date and time based on city

--- src/main/java/com/example/application/components/DeliveryForm.java
+++ src/main/java/com/example/application/components/DeliveryForm.java
@@ -13,6 +13,7 @@ import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.value.ValueChangeMode;
 
 public class DeliveryForm extends BasicForm {
+    private Binder<Order> binder;
     private RadioButtonGroup<String> method;
     private ComboBox<String> city;
     private TextField address;
@@ -20,37 +21,66 @@ public class DeliveryForm extends BasicForm {
     private TextArea comment;
 
     public DeliveryForm(Binder<Order> binder) {
+        this.binder = binder;
+
+        addMethodField();
+        addCityField();
+        addAddressField();
+        addDateTimeField();
+        addCommentField();
+    }
+
+    private void addMethodField() {
         method = new RadioButtonGroup<>("Method");
         method.setItems("Posti", "UPS", "Home");
         binder.forField(method)
                 .asRequired("The field is required")
                 .bind("delivery.method");
         add(method);
+    }
 
+    private void addCityField() {
         city = new ComboBox<>("City");
-        city.setItems(List.of("Helsinki", "Turku", "Tampere"));
+        city.setItems(List.of("Helsinki", "Turku"));
         city.addValueChangeListener(event -> {
-            // dateTime.setMin();
+            String value = event.getValue();
+            LocalDateTime now = LocalDateTime.now().withMinute(0).withSecond(0).withNano(0);
+
+            dateTime.setEnabled(true);
+
+            if (value.equals("Helsinki")) {
+                dateTime.setMin(now.plusDays(0).withHour(10));
+                dateTime.setMax(now.plusDays(10).withHour(22));
+            } else if (value.equals("Turku")) {
+                dateTime.setMin(now.plusDays(20).withHour(10));
+                dateTime.setMax(now.plusDays(30).withHour(22));
+            }
         });
         binder.forField(city)
                 .asRequired("The field is required")
                 .bind("delivery.city");
         add(city);
+    }
 
+    private void addAddressField() {
         address = new TextField("Address");
         address.setHelperText("Example: postal code, district, street, apartment");
         binder.forField(address)
                 .asRequired("The field is required")
                 .bind("delivery.address");
         add(address);
+    }
 
+    private void addDateTimeField() {
         dateTime = new DateTimePicker("Suitable date and time");
-        dateTime.setMin(LocalDateTime.now());
+        dateTime.setEnabled(false);
         binder.forField(dateTime)
                 .asRequired("The field is required")
                 .bind("delivery.dateTime");
         add(dateTime);
+    }
 
+    private void addCommentField() {
         comment = new TextArea("Additional information (optional)");
         comment.setMaxLength(350);
         comment.setValueChangeMode(ValueChangeMode.EAGER);
